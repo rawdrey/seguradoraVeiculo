09b15fdee1e4d6f11c4456a2fc257570d0e2cd1a
Rawdrey <rawdrey@gmail.com>
2024-05-19 09:58:38 -0300

    inicializadas class veiculo, e as class carro, moto,moto, fazem poliformismo.

---
+++ seguradoraVeiculo/Veiculo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seguradoraVeiculo
+{
+    class Veiculo
+    {
+public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public double ValorVeiculo { get; set; }
+        public virtual double CalcularSeguro()
+        {
+            return ValorVeiculo * 0.05;
+        }
+    }
+}
